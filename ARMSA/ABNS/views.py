b87b855af1eec35dc1d6cfb6ebe2f8d1f29bd42a
willburninhell <willburninhell@gmail.com>
2013-12-05 05:10:39 +0200

    todo: add remove

--- ARMSA/ABNS/views.py
+++ ARMSA/ABNS/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import client, street
 
 
-def streets_view(request,st="Ba",ho="1"):
+def streets_view(request, st="Ba", ho="1"):
     streets_query = street.objects.all().order_by('long_name')
     clients_query = client.objects.all()
     address_query = client.objects.filter(street=st).filter(house=ho)
@@ -29,34 +29,36 @@ def streets_view(request,st="Ba",ho="1"):
     return render(request, "streettree.html", {'streets_query': streets_query, 'clients_query': clients_query, 'address_query': address_query})
 
 
-def abns(request):
-    streets_query = street.objects.all().order_by('long_name')
+def search(request):
+    if request.method == 'POST':
+        postdata = request.POST
+        dogovor = postdata['dogovor']
+        ip = postdata['ip']
+        mac = postdata['mac']
+
+    streets = street.objects.all().order_by('long_name')
     clients_query = client.objects.all()
+    results_query = client.objects.all()
+    if (dogovor != ""):
+        results_query = results_query.filter(dogovor__contains=dogovor)
+    if (ip != ""):
+        results_query = results_query.filter(ip__contains=ip)
+    if (mac != ""):
+        results_query = results_query.filter(mac__contains=mac)
     #form = UserRowForm()
-    return render(request, "abns.html", {'streets_query': streets_query, 'clients_query': clients_query})
+
+    return render(request, "results.html", {'streets': streets, 'clients_query': clients_query, 'results_query': results_query})
 
 
-def clients(request,st="Ba",ho="1"):
+def clients(request, st="Ba", ho="1"):
     entries = client.objects.filter(street=st, house=ho).extra(
         select={'flatint': 'CAST(REPLACE(REPLACE(REPLACE(REPLACE(flat,"b",""),"B",""),"a",""),"A","") AS UNSIGNED)'}).order_by('flatint')
     streets = street.objects.all()
-    #form = UserRowForm()
-
-    if request.method == 'POST':
-        postdata = request.POST
-        # print postdata['id']
-        change_client = client.objects.get(id=postdata['id'])
-        change_client.ip = postdata['ip']
-        if change_client.ip[3] in '1234':
-            change_client.entrance = int(change_client.ip[3])
-        else:
-            change_client.entrance = 51
-        change_client.main_ip = postdata['main_ip']
-        change_client.dogovor = postdata['dogovor']
-        change_client.street = postdata['street']
-        change_client.house = postdata['house']
-        change_client.flat = postdata['flat']
-        change_client.save(
-            update_fields=['ip', 'main_ip', 'dogovor', 'street', 'house', 'flat', 'entrance'])
-
     return render(request, "clients.html", {'entries': entries, 'streets': streets})
+
+
+def delete_id(request, del_id, st="Ba", ho="1"):
+    i = int(del_id)
+    e = client.objects.get(id=i)
+    e.delete()
+    return HttpResponse('OK')
